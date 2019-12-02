d398087256d22b93cc64c49e487229952b946770
Elisa Staiti <eli.viviana@gmail.com>
2019-12-02 16:32:38 -0300

    correccion de formulario y validaciones

---
+++ primerintento/js/header.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+  var titulo = document.querySelector(".title");
+  console.log(titulo);
+  titulo.style.color="#fff";
+let boton = document.querySelector(".btnheader");
+let objeto = document.querySelector(".objheader");
+console.log(objeto);
+console.log(boton);
+
+// boton.onclick = function(){
+  // objeto.style.height = '500px';
+// }
+
+
+}

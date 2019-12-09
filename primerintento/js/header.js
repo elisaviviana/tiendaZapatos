66a9f5b87d2f138ece3822776127b47c3971479a
Elisa Staiti <eli.viviana@gmail.com>
2019-12-09 16:15:52 -0300

    observacionesSprint2

--- primerintento/js/header.js
+++ primerintento/js/header.js
@@ -1,16 +1,15 @@
 window.onload = function(){
 
-  var titulo = document.querySelector(".title");
-  console.log(titulo);
-  titulo.style.color="#fff";
-let boton = document.querySelector(".btnheader");
-let objeto = document.querySelector(".objheader");
-console.log(objeto);
-console.log(boton);
+  var divheader = document.querySelector(".menus");
+  //var boton = document.querySelector(".btnheader");
 
-// boton.onclick = function(){
-  // objeto.style.height = '500px';
-// }
+  console.log(divheader);
+//  console.log(boton);
 
-
+  divheader.onclick = function () {
+    divheader.style.height = "100vh";
+  }
+//  boton.onclick = function(){
+//    divheader.style.zindex = "100%";
+//    }
 }

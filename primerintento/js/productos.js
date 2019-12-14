cd95bd4603e37d2f58e8082ea766b56fc54192ff
Elisa <elisas@itcsa.net.ar>
2019-12-14 12:28:31 -0300

    json

--- primerintento/js/productos.js
+++ primerintento/js/productos.js
@@ -1,21 +1,111 @@
 //fetch
-fetch('https://my-json-server.typicode.com/elisaviviana/jsonDH')
+// fetch('http://my-json-server.typicode.com/elisaviviana/jsonDH')
+
+fetch('https://my-json-server.typicode.com/elisaviviana/jsonDH/productos')
    .then(function(respuesta){
       return respuesta.json();
    })
    .then(function(dataCruda){
+     //console.log(dataCruda);
      // var lista = document.querySelector('ul');
      // var nuevoli = document.createElement('li');
      // var textoDelLi = document.createElement('soy un elemento');
      //
      // nuevoli.append(textoDelLi);
      // lista.append(nuevoli);
+ let produc = document.querySelector('.jsonproducto');
+ let nuevorow = document.createElement('div');
+ nuevorow.className = "row ";
+ produc.append(nuevorow);
 
 
-      let produc = document.querySelector('.jsonproducto');
-      let div1 = document.createElement('div');
-      div1.className += " col-md-4 col-sm-4 ";
-      produc.append(div1);
+dataCruda.forEach(function(elemento){
+   // console.log(dataCruda[0]['id']);
+
+
+if(elemento['id']%3==0){
+  console.log("entro al ig ==0");
+ let nuevorow = document.createElement('div');
+ nuevorow.className = "row ";
+ produc.append(nuevorow);
+
+dataCruda.forEach(function(elemento){
+
+     let nuevodiv = document.createElement('div');
+      nuevodiv.className+="col-md-4 col-sm-4";
+     console.log(nuevodiv);
+     let nuevodiv2=document.createElement('div');
+     nuevodiv2.className+="header-content-left";
+     nuevodiv.append(nuevodiv2);
+     let img=document.createElement('img');
+     img.src+=src=elemento['img'];
+     img.style=style="width: 100%;"
+     nuevodiv2.append(img);
+     let nuevoh4=document.createElement('h4');
+     nuevoh4.className+="display-5";
+     nuevoh4.innerHTML='Descripcion: '+elemento['desc'];
+     nuevodiv2.append(nuevoh4);
+     nuevop=document.createElement('p');
+     nuevop.className+="mt-2";
+     nuevop.innerHTML="Precio: $"+elemento['precio'];
+     nuevodiv2.append(nuevop);
+
+     nuevodiv3=document.createElement('div');
+     nuevodiv3.className="mb-2 bg-success text-white text-center font-weight-bold text-decoration-none";
+
+     nuevoa=document.createElement('a');
+     nuevoa.className="text-reset";
+     nuevoa.href="descripcion.html";
+     nuevoa.innerHTML='Agregar al Carrito';
+     nuevodiv3.append(nuevoa);
+     nuevodiv2.append(nuevodiv3);
+     nuevorow.append(nuevodiv);
+        console.log(produc);
+
+
+});
+}else{
+  console.log("entro al ig !=0");
+
+  dataCruda.forEach(function(elemento){
+
+       let nuevodiv = document.createElement('div');
+        nuevodiv.className+="col-md-4 col-sm-4";
+       console.log(nuevodiv);
+       let nuevodiv2=document.createElement('div');
+       nuevodiv2.className+="header-content-left";
+       nuevodiv.append(nuevodiv2);
+       let img=document.createElement('img');
+       img.src+=src=elemento['img'];
+       img.style=style="width: 100%;"
+       nuevodiv2.append(img);
+       let nuevoh4=document.createElement('h4');
+       nuevoh4.className+="display-5";
+       nuevoh4.innerHTML='Descripcion: '+elemento['desc'];
+       nuevodiv2.append(nuevoh4);
+       nuevop=document.createElement('p');
+       nuevop.className+="mt-2";
+       nuevop.innerHTML="Precio: $"+elemento['precio'];
+       nuevodiv2.append(nuevop);
+
+       nuevodiv3=document.createElement('div');
+       nuevodiv3.className="mb-2 bg-success text-white text-center font-weight-bold text-decoration-none";
+
+       nuevoa=document.createElement('a');
+       nuevoa.className="text-reset";
+       nuevoa.href="descripcion.html";
+       nuevoa.innerHTML='Agregar al Carrito';
+       nuevodiv3.append(nuevoa);
+       nuevodiv2.append(nuevodiv3);
+       nuevorow.append(nuevodiv);
+          console.log(produc);
+
+
+  });
+}
+ });
+
+
 
       // dataCruda.forEach(function(elemento){
       //    var pais = elemento['name'];
@@ -29,16 +119,3 @@ fetch('https://my-json-server.typicode.com/elisaviviana/jsonDH')
    .catch(function(error){
       console.log('Descripción del Error: ' + error);
 });
-
-
-
-// <div class="col-md-4 col-sm-4 ">
-//   <div class="header-content-left">
-//     <img src="img/image1.jpg" style="width: 100%;">
-//     <h4 class="display-5">Descripcion</h4>
-//     <p class="mt-2">Numero: </p>
-//
-//     <div class="mb-2 bg-success text-white text-center font-weight-bold text-decoration-none"><a href="descripcion.html" class="text-reset">Agregar al Carrito</a></div>
-//
-//   </div>
-// </div>

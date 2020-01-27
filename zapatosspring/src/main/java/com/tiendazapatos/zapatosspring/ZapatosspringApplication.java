afc29b44fc0630ea20332a115295da5986c5a78a
Benjamin Toledo Sacchi <benjasacchi@yahoo.com.ar>
2020-01-27 20:04:18 -0300

    proyecto spring

---
+++ zapatosspring/src/main/java/com/tiendazapatos/zapatosspring/ZapatosspringApplication.java
@@ -0,0 +1,13 @@
+package com.tiendazapatos.zapatosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapatosspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapatosspringApplication.class, args);
+	}
+
+}
